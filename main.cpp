41e495f05496ebc91d4b25582ab4255e4b2662a2
23026850 <142709188+23026850@users.noreply.github.com>
2024-03-16 01:53:12 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,93 +1,22 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    double merchandiseCost,storeRent,employeesSalary,electricityCost,expense,actualProfit,changeNetProfit,finalMerchandise;
-    cout << "Enter the total cost of the merchandise: " << endl;
-    cin >> merchandiseCost;
-    cout << " Enter the total salary of the employees including yours: " << endl;
-    cin >> employeesSalary;
-    cout << " Enter the yearly rent: " << endl;
-    cin >> storeRent;
-    cout << " Enter the estimated electricity cost: " << endl;
-    cin >> electricityCost;
+    double d,h,angle,height;
 
-    expense= merchandiseCost+ employeesSalary+ storeRent+ electricityCost;
-    cout << "Your expense is: " << expense << endl;
-    actualProfit= merchandiseCost*0.1;
-    cout << "Your acual profit rate is: " << actualProfit << endl;
-    changeNetProfit= actualProfit/0.85
-    finalMerchandise=(changeNetProfit+ expense)/merchandiseCost;
-    cout << "The final merchandise cost is: " << finalMerchandise << endl;
+    cout << "Enter the horizontal distance between you and the tree: " << endl;
+    cin >> d;
+
+    cout << "Enter the height at sight: " << endl;
+    cin >> h;
+
+    cout << "Enter the angle on the angle-measuring device: " << endl;
+    cin >> angle;
+
+    height= h+d*tan(angle);
+    cout << "The actual height of the tree is: " << height;
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
